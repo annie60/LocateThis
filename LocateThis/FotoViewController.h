4d97bfaea72ddf0df45fdb2f95d988e661530764
Ana Arellano <a01089996@itesm.mx>
2014-10-20 23:41:12 -0500

    Cambios en el layout
    ultimas correcciones según lo que vimos en la junta, los recursos son
    tentativos, después se pueden modificar.

---
+++ LocateThis/FotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  FotoViewController.h
+//  LocateThis
+//
+//  Created by MacBook on 10/20/14.
+//  Copyright (c) 2014 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FotoViewController : UIViewController
+
+@end
---
+++ LocateThis/MapaViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapaViewController.h
+//  LocateThis
+//
+//  Created by MacBook on 10/20/14.
+//  Copyright (c) 2014 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapaViewController : UIViewController
+
+@end
