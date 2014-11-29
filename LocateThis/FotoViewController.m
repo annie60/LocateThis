a46ea622b5d368f4d6c88d55eaa1daa4ef52a7ff
dquilla <David@MacBook-Pro-de-Lourdes.local>
2014-11-29 15:12:57 -0600

    Cosas

--- LocateThis/FotoViewController.m
+++ LocateThis/FotoViewController.m
@@ -90,22 +90,21 @@
 
 - (IBAction)buscar:(id)sender {
     // Get the path to the Documents folder
-           NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-          NSString *documentDirectoryPath = [paths objectAtIndex:0];
-    //
-    //        // Get the path to an file named "tmp_image.jpg" in the Documents folder
-           NSString *imagePath = [documentDirectoryPath stringByAppendingPathComponent:@"tmp_image.jpg"];
-           NSURL *imageURL = [NSURL fileURLWithPath:imagePath];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentDirectoryPath = [paths objectAtIndex:0];
+    // Get the path to an file named "tmp_image.jpg" in the Documents folder
+    NSString *imagePath = [documentDirectoryPath stringByAppendingPathComponent:@"tmp_image.jpg"];
+    NSURL *imageURL = [NSURL fileURLWithPath:imagePath];
     
     // Write the image to an file called "tmp_image.jpg" in the Documents folder
     NSData *imageData = UIImageJPEGRepresentation(self.ImagenFoto.image, 1.0);
     [imageData writeToURL:imageURL atomically:YES];
     
-  
-    NSDictionary* parameters = @{@"focus[x]": @"480", @"focus[y]": @"640", @"image_request[altitude]": @"27.912109375", @"image_request[language]": @"en", @"image_request[latitude]": @"35.8714220766008", @"image_request[locale]": @"en_US", @"image_request[longitude]": @"14.3583203002251", @"image_request[image]": imageURL};
+    NSDictionary* parameters = @{ @"focus[x]": @"480", @"focus[y]": @"640", @"image_request[altitude]": @"27.912109375", @"image_request[language]": @"en", @"image_request[latitude]": @"35.8714220766008", @"image_request[locale]": @"en_US", @"image_request[longitude]": @"14.3583203002251", @"image_request[image]": imageURL};
 
     // And the headers
     NSDictionary* headers = @{@"X-Mashape-Key":@"evGDsuPOjemsh7rcxyGVnnNVZoZJp16Ecurjsno4pspMAHqplY",  @"Content-Type": @"application/x-www-form-urlencoded"};
+    
     NSError *error;
     // Call the API using Unirest
     UNIUrlConnection *asyncConnection = [[UNIRest post:^(UNISimpleRequest *request) {
@@ -121,10 +120,10 @@
         //NSDictionary *datos = [NSJSONSerialization JSONObjectWithData:[response.body.JSONObject objectForKey:@"token"]options:kNilOptions error:&error];
         //NSMutableArray*respuesta =[[NSMutableArray alloc]init];
         //respuesta=[datos objectForKey:@"token"];
+        
         NSDictionary *headers1 = @{@"X-Mashape-Key": @"evGDsuPOjemsh7rcxyGVnnNVZoZJp16Ecurjsno4pspMAHqplY"};
         NSString*liga=[NSString stringWithFormat:@"https://camfind.p.mashape.com/image_responses/%@",[response.body.JSONObject objectForKey:@"token"]];
         UNIUrlConnection *asyncConnection = [[UNIRest get:^(UNISimpleRequest *request) {
-            
             [request setUrl:liga];
             [request setHeaders:headers1];
         }] asJsonAsync:^(UNIHTTPJsonResponse *response, NSError *error) {
@@ -137,8 +136,5 @@
             ];
         
     }];
-    
-   
-
 }
 @end
